67d116d64cc8a3d99e3a440a8d3acd33e473d97d
Scott W Harden <swharden@gmail.com>
2018-09-23 17:44:52 -0400

    RGB channels are additive

---
+++ src/sandbox2/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace sandbox2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkRed = new System.Windows.Forms.CheckBox();
+            this.checkGreen = new System.Windows.Forms.CheckBox();
+            this.checkBlue = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox1.Location = new System.Drawing.Point(129, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(374, 326);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // checkRed
+            // 
+            this.checkRed.AutoSize = true;
+            this.checkRed.Location = new System.Drawing.Point(13, 129);
+            this.checkRed.Name = "checkRed";
+            this.checkRed.Size = new System.Drawing.Size(46, 17);
+            this.checkRed.TabIndex = 1;
+            this.checkRed.Text = "Red";
+            this.checkRed.UseVisualStyleBackColor = true;
+            this.checkRed.CheckedChanged += new System.EventHandler(this.checkRed_CheckedChanged);
+            // 
+            // checkGreen
+            // 
+            this.checkGreen.AutoSize = true;
+            this.checkGreen.Location = new System.Drawing.Point(13, 152);
+            this.checkGreen.Name = "checkGreen";
+            this.checkGreen.Size = new System.Drawing.Size(55, 17);
+            this.checkGreen.TabIndex = 2;
+            this.checkGreen.Text = "Green";
+            this.checkGreen.UseVisualStyleBackColor = true;
+            this.checkGreen.CheckedChanged += new System.EventHandler(this.checkGreen_CheckedChanged);
+            // 
+            // checkBlue
+            // 
+            this.checkBlue.AutoSize = true;
+            this.checkBlue.Location = new System.Drawing.Point(13, 175);
+            this.checkBlue.Name = "checkBlue";
+            this.checkBlue.Size = new System.Drawing.Size(47, 17);
+            this.checkBlue.TabIndex = 3;
+            this.checkBlue.Text = "Blue";
+            this.checkBlue.UseVisualStyleBackColor = true;
+            this.checkBlue.CheckedChanged += new System.EventHandler(this.checkBlue_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 424);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBlue);
+            this.Controls.Add(this.checkGreen);
+            this.Controls.Add(this.checkRed);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkRed;
+        private System.Windows.Forms.CheckBox checkGreen;
+        private System.Windows.Forms.CheckBox checkBlue;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ src/sandbox2/Frames.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sandbox2
+{
+    /// <summary>
+    /// A frame is a single collection of pixel values (int16 depth)
+    /// </summary>
+    class Frame
+    {
+        public int displayMin;
+        public int displayMax;
+        public string color;
+        public Size size;
+        public UInt16[] pixels;
+
+        public Frame(UInt16[] pixels, Size size, string color = "gray", int displayMin = 0, int displayMax = 54321)
+        {
+            this.pixels = pixels;
+            this.size = size;
+            this.color = color;
+            this.displayMin = displayMin;
+            this.displayMax = displayMax;
+        }
+    }
+
+    class Frames
+    {
+        public Bitmap bmpOriginal;
+        public Frame[] frames;
+        public Frames(string imageFilePath)
+        {
+            bmpOriginal = new Bitmap(imageFilePath);
+
+            // create int16 arrays of pixels for R, G, and B channels
+            int nPixels = bmpOriginal.Width * bmpOriginal.Height;
+            byte[] bytes = BitmapToBytes(bmpOriginal);
+            UInt16[] pixelsR = new UInt16[nPixels];
+            UInt16[] pixelsG = new UInt16[nPixels];
+            UInt16[] pixelsB = new UInt16[nPixels];
+            for (int pixel = 0; pixel < nPixels; pixel++)
+            {
+                pixelsR[pixel] = bytes[3 * pixel + 2];
+                pixelsG[pixel] = bytes[3 * pixel + 1];
+                pixelsB[pixel] = bytes[3 * pixel + 0];
+            }
+
+            // load these arrays into frames
+            frames = new Frame[3];
+            frames[0] = new Frame(pixelsR, bmpOriginal.Size);
+            frames[1] = new Frame(pixelsG, bmpOriginal.Size);
+            frames[2] = new Frame(pixelsB, bmpOriginal.Size);
+        }
+
+        /// <summary>
+        /// return a bitmap (of any image format) as a byte array of its data values
+        /// </summary>
+        public byte[] BitmapToBytes(Bitmap bmp)
+        {
+            int bytesPerPixel = Image.GetPixelFormatSize(bmp.PixelFormat) / 8;
+            byte[] bytes = new byte[bmp.Width * bmp.Height * bytesPerPixel];
+            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            BitmapData bmpData = bmp.LockBits(rect, ImageLockMode.ReadOnly, bmp.PixelFormat);
+            Marshal.Copy(bmpData.Scan0, bytes, 0, bytes.Length);
+            bmp.UnlockBits(bmpData);
+            return bytes;
+        }
+
+        /// <summary>
+        /// create a bitmap given a byte array of raw data
+        /// </summary>
+        public Bitmap BitmapFromBytes(byte[] bytes, Size size, PixelFormat format = PixelFormat.Format24bppRgb)
+        {
+            Bitmap bmp = new Bitmap(size.Width, size.Height, format);
+            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            BitmapData bmpData = bmp.LockBits(rect, ImageLockMode.ReadWrite, bmp.PixelFormat);
+            Marshal.Copy(bytes, 0, bmpData.Scan0, bytes.Length);
+            bmp.UnlockBits(bmpData);
+            return bmp;
+        }
+
+        public Bitmap AddColor(Bitmap bmp, UInt16[] pixels, string color = "gray")
+        {
+            byte[] bytes = BitmapToBytes(bmp);
+            for (int i = 0; i < pixels.Length; i++)
+            {
+                byte val = (byte)pixels[i];
+                if (color == "gray")
+                {
+                    bytes[i * 3 + 2] = val; // red
+                    bytes[i * 3 + 1] = val; // green
+                    bytes[i * 3 + 0] = val; // blue
+                } else if (color == "red")
+                {
+                    bytes[i * 3 + 2] = val; // red
+                }
+                else if (color == "green")
+                {
+                    bytes[i * 3 + 1] = val; // green
+                }
+                else if (color == "blue")
+                {
+                    bytes[i * 3 + 0] = val; // blue
+                }
+                else if (color == "magenta")
+                {
+                    bytes[i * 3 + 2] = val; // red
+                    bytes[i * 3 + 0] = val; // blue
+                }
+            }
+            bmp = BitmapFromBytes(bytes, bmp.Size);
+            return bmp;
+        }
+
+        public Bitmap CreateMerged()
+        {
+            Bitmap bmp = new Bitmap(bmpOriginal.Width, bmpOriginal.Height, PixelFormat.Format24bppRgb);
+            bmp = AddColor(bmp, frames[0].pixels,"red");
+            bmp = AddColor(bmp, frames[1].pixels, "green");
+            bmp = AddColor(bmp, frames[2].pixels, "blue");
+            return bmp;
+        }
+    }
+}
