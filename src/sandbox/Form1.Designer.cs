e03b9d8e89e88df471c82efeb649daa6633e323e
Scott W Harden <swharden@gmail.com>
2018-09-23 08:12:15 -0400

    RGB sandbox

---
+++ src/sandbox/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace sandbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.hScrollChannel = new System.Windows.Forms.HScrollBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.hScrollSlice = new System.Windows.Forms.HScrollBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(570, 477);
+            this.panel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(568, 475);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DimGray;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(562, 409);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 5;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 75F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 75F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 75F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 75F));
+            this.tableLayoutPanel2.Controls.Add(this.comboBox1, 4, 0);
+            this.tableLayoutPanel2.Controls.Add(this.hScrollChannel, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label5, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.comboBox5, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.checkBox1, 3, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 418);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(562, 24);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // hScrollChannel
+            // 
+            this.hScrollChannel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.hScrollChannel.LargeChange = 1;
+            this.hScrollChannel.Location = new System.Drawing.Point(75, 0);
+            this.hScrollChannel.Maximum = 2;
+            this.hScrollChannel.Name = "hScrollChannel";
+            this.hScrollChannel.Size = new System.Drawing.Size(262, 24);
+            this.hScrollChannel.TabIndex = 0;
+            this.hScrollChannel.ValueChanged += new System.EventHandler(this.hScrollChannel_ValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Location = new System.Drawing.Point(3, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 24);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "C: 1/3";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBox5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "Gray",
+            "Red",
+            "Green",
+            "Blue",
+            "Magenta"});
+            this.comboBox5.Location = new System.Drawing.Point(340, 3);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(69, 21);
+            this.comboBox5.TabIndex = 2;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 75F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.label6, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.hScrollSlice, 1, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 448);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(562, 24);
+            this.tableLayoutPanel3.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label6.Location = new System.Drawing.Point(3, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 24);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Z: 1/22";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // hScrollSlice
+            // 
+            this.hScrollSlice.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.hScrollSlice.LargeChange = 1;
+            this.hScrollSlice.Location = new System.Drawing.Point(75, 0);
+            this.hScrollSlice.Maximum = 11;
+            this.hScrollSlice.Name = "hScrollSlice";
+            this.hScrollSlice.Size = new System.Drawing.Size(487, 24);
+            this.hScrollSlice.TabIndex = 1;
+            this.hScrollSlice.ValueChanged += new System.EventHandler(this.hScrollSlice_ValueChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Merge",
+            "Grayscale",
+            "Composite"});
+            this.comboBox1.Location = new System.Drawing.Point(487, 3);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(0, 3, 3, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(72, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkBox1.Location = new System.Drawing.Point(415, 3);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(69, 18);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "visible";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 477);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.HScrollBar hScrollChannel;
+        private System.Windows.Forms.HScrollBar hScrollSlice;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ src/sandbox/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sandbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SetSliceAndChannelFromScrollBars();
+            LoadRGBImage();
+            SetSliceAndChannel(0, 0);
+        }
+
+        public Bitmap[] bmpFrames;
+        public void LoadRGBImage()
+        {
+            string filePath = @"D:\demoData\tifs\simple\rgb-test2.jpg";
+            Bitmap bmpRGB = new Bitmap(filePath);
+            bmpFrames = new Bitmap[3];
+            bmpFrames[0] = ExtractChannelToGrayscale(bmpRGB, 0);
+            bmpFrames[1] = ExtractChannelToGrayscale(bmpRGB, 1);
+            bmpFrames[2] = ExtractChannelToGrayscale(bmpRGB, 2);
+        }
+
+        /// <summary>
+        /// return a bitmap (of any image format) as a byte array of its data values
+        /// </summary>
+        public byte[] BitmapToBytes(Bitmap bmp)
+        {
+            int bytesPerPixel = Image.GetPixelFormatSize(bmp.PixelFormat) / 8;
+            byte[] bytes = new byte[bmp.Width * bmp.Height * bytesPerPixel];
+            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            BitmapData bmpData = bmp.LockBits(rect, ImageLockMode.ReadOnly, bmp.PixelFormat);
+            Marshal.Copy(bmpData.Scan0, bytes, 0, bytes.Length);
+            bmp.UnlockBits(bmpData);
+            return bytes;
+        }
+
+        /// <summary>
+        /// create a bitmap given a byte array of raw data
+        /// </summary>
+        public Bitmap BitmapFromBytes(byte[] bytes, int width, int height, PixelFormat format = PixelFormat.Format8bppIndexed)
+        {
+            Bitmap bmp = new Bitmap(width, height, format);
+            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            BitmapData bmpData = bmp.LockBits(rect, ImageLockMode.ReadWrite, bmp.PixelFormat);
+            Marshal.Copy(bytes, 0, bmpData.Scan0, bytes.Length);
+            bmp.UnlockBits(bmpData);
+            return bmp;
+        }
+
+        /// <summary>
+        /// Apply red, green, blue, magenta, or gray LUT to an 8-bit indexed bitmap
+        /// </summary>
+        public void BitmapApplyPalette(Bitmap bmp, string color = "gray")
+        {
+            ColorPalette pal = bmp.Palette;
+            for (int i = 0; i < 256; i++)
+            {
+                if (color == "red")
+                    pal.Entries[i] = System.Drawing.Color.FromArgb(255, i, 0, 0);
+                else if (color == "green")
+                    pal.Entries[i] = System.Drawing.Color.FromArgb(255, 0, i, 0);
+                else if (color == "blue")
+                    pal.Entries[i] = System.Drawing.Color.FromArgb(255, 0, 0, i);
+                else if (color == "magenta")
+                    pal.Entries[i] = System.Drawing.Color.FromArgb(255, i, 0, i);
+                else if (color == "gray")
+                    pal.Entries[i] = System.Drawing.Color.FromArgb(255, i, i, i);
+                else
+                    Console.WriteLine($"Unknown color pallette code: {color}");
+            }
+            bmp.Palette = pal;
+        }
+
+        public Bitmap ExtractChannelToGrayscale(Bitmap bmpSource, int channel = 0)
+        {
+            // copy our source image to a byte array (8-bits/pixel, 3 channel RGB)
+            byte[] bytesRGB = BitmapToBytes(bmpSource);
+            int nPixels = bytesRGB.Length / 3;
+
+            // create a byte array for out grayscale image (16-bits/pixel, 1 channel)
+            byte[] bytesGray = new byte[nPixels];
+
+            for (int pixel = 0; pixel < nPixels; pixel++)
+            {
+                bytesGray[pixel] = bytesRGB[3 * pixel + (2 - channel)];
+            }
+
+            // make a 16-bit grayscale image the same size as the source
+            Bitmap bmpGray = BitmapFromBytes(bytesGray, bmpSource.Width, bmpSource.Height);
+
+            if (channel==0)
+                BitmapApplyPalette(bmpGray,"red");
+            else if (channel == 1)
+                BitmapApplyPalette(bmpGray, "green");
+            else if (channel == 2)
+                BitmapApplyPalette(bmpGray, "blue");
+
+            return bmpGray;
+        }
+
+        public void SetSliceAndChannel(int slice, int channel)
+        {
+            if (bmpFrames == null)
+                return;
+            pictureBox1.Image = bmpFrames[channel];
+        }
+
+        public void SetSliceAndChannelFromScrollBars()
+        {
+            SetSliceAndChannel(hScrollSlice.Value, hScrollChannel.Value);
+        }
+
+        private void hScrollChannel_Scroll(object sender, ScrollEventArgs e)
+        {
+            SetSliceAndChannelFromScrollBars();
+        }
+
+        private void hScrollChannel_ValueChanged(object sender, EventArgs e)
+        {
+            SetSliceAndChannelFromScrollBars();
+        }
+
+        private void hScrollSlice_ValueChanged(object sender, EventArgs e)
+        {
+            SetSliceAndChannelFromScrollBars();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
