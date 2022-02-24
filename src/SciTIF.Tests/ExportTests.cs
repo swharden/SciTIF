bdbc5a47fe6c7d8263aff8284ee320defc6bd59b
Scott W Harden <swharden@gmail.com>
2022-02-24 01:13:39 -0500

    Export: PNG()

---
+++ src/SciTIF.Tests/ExportTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SciTIF.Tests
+{
+    internal class ExportTests
+    {
+        [Test]
+        public void Test_Export_AllTestImages()
+        {
+            string outputFolder = Path.GetFullPath(Path.Combine(TestContext.CurrentContext.TestDirectory, "output"));
+            if (!Directory.Exists(outputFolder))
+                Directory.CreateDirectory(outputFolder);
+
+            foreach (string tifPath in SampleData.TifFiles)
+            {
+                TifFile tif = new(tifPath);
+                Console.WriteLine(tif);
+                string outputPath = Path.Combine(outputFolder, Path.GetFileName(tifPath)+".png");
+                Console.WriteLine(outputPath);
+                Export.PNG(outputPath, Adjust.AutoScale(tif.Values));
+            }
+        }
+    }
+}
---
+++ src/SciTIF/Export.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace SciTIF;
+
+public static class Export
+{
+    public static void PNG(string filePath, double[,] values)
+    {
+        using Bitmap bmp = GetBitmap(values);
+        bmp.Save(filePath, System.Drawing.Imaging.ImageFormat.Png);
+    }
+
+    private static byte Clamp(double x, byte min = 0, byte max = 255)
+    {
+        if (x < min)
+            return min;
+        else if (x > max)
+            return max;
+        else
+            return (byte)x;
+    }
+
+    // TODO: implement with SkiaSharp
+    private static Bitmap GetBitmap(double[,] values)
+    {
+        int width = values.GetLength(1);
+        int height = values.GetLength(0);
+        int stride = (width % 4 == 0) ? width : width + 4 - width % 4;
+
+        byte[] pixelsOutput = new byte[stride * height];
+        for (int y = 0; y < height; y++)
+            for (int x = 0; x < width; x++)
+                pixelsOutput[y * stride + x] = Clamp(values[y, x]);
+
+        var formatOutput = System.Drawing.Imaging.PixelFormat.Format8bppIndexed;
+
+        var rect = new Rectangle(0, 0, width, height);
+        Bitmap bmp = new(stride, height, formatOutput);
+        System.Drawing.Imaging.BitmapData bmpData = bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadOnly, formatOutput);
+        System.Runtime.InteropServices.Marshal.Copy(pixelsOutput, 0, bmpData.Scan0, pixelsOutput.Length);
+        bmp.UnlockBits(bmpData);
+
+        System.Drawing.Imaging.ColorPalette pal = bmp.Palette;
+        for (int i = 0; i < 256; i++)
+            pal.Entries[i] = System.Drawing.Color.FromArgb(255, i, i, i);
+        bmp.Palette = pal;
+
+        return bmp;
+    }
+}
