773762e0a6dcdad48e5f4b0b22bdaebf432398f0
Scott W Harden <swharden@gmail.com>
2022-02-27 21:54:30 -0500

    Test: save every frame of a stack

---
+++ src/SciTIF.Tests/StackTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SciTIF.Tests
+{
+    internal class StackTests
+    {
+        [Test]
+        public void Test_Stack_SaveImages()
+        {
+            string outputFolder = Path.GetFullPath("_multiChannelFrames");
+            if (!Directory.Exists(outputFolder))
+                Directory.CreateDirectory(outputFolder);    
+
+            string filePath = Path.Combine(SampleData.DataFolder, "C3Z4F5.tif");
+            TifFile tif = new(filePath);
+            Console.WriteLine(tif.FormatDescription);
+
+            for (int i = 0; i < tif.Channels.Length; i++)
+            {
+                string outputFilePath = Path.Combine(outputFolder, $"test-stack-{i:000}.png");
+                Console.WriteLine(outputFilePath);
+                tif.Channels[i].SavePng(outputFilePath);
+            }
+        }
+    }
+}
