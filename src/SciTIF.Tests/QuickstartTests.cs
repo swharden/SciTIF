71bc35a0eda2d01a9aa47d47e652cfa7407d8ab4
Scott W Harden <swharden@gmail.com>
2022-08-25 23:05:42 -0400

    quickstart: demonstrate 16-bit tif autoscaling

--- src/SciTIF.Tests/QuickstartTests.cs
+++ src/SciTIF.Tests/QuickstartTests.cs
@@ -12,11 +12,11 @@ namespace SciTIF.Tests
         [Test]
         public void Test_Quickstart()
         {
-            string path = SampleData.TifWithFramesSlicesAndChannels;
+            string path = System.IO.Path.Combine(SampleData.DataFolder, "baboon 16bit grayscale.tif");
             TifFile tif = new(path);
-            Image slice = tif.GetImage(0, 0, 0);
-            slice.AutoScale();
-            slice.SavePng("test");
+            Image slice = tif.GetImage(0, 0, 0); // pixel values exceed 255
+            slice.AutoScale(); // scale pixel values down to 0-255
+            slice.SavePng("baboon-16.png");
         }
     }
 }
