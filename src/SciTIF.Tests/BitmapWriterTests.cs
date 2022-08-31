470992cfb6c5f8f5b11b81492683110771930260
Scott W Harden <swharden@gmail.com>
2022-08-30 23:41:20 -0400

    create native Bitmap writer

---
+++ src/SciTIF.Tests/BitmapWriterTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SciTIF.Tests
+{
+    internal class BitmapWriterTests
+    {
+        [Test]
+        public void Test_Write_File()
+        {
+            int width = 17;
+            int height = 15;
+
+            byte[,,] pixels = new byte[height, width, 4];
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    byte r = (byte)(x * 255 / width);
+                    byte g = (byte)(y * 255 / height);
+                    byte b = (byte)((r + g) / 2);
+                    byte a = 255;
+
+                    pixels[y, x, 0] = r;
+                    pixels[y, x, 1] = g;
+                    pixels[y, x, 2] = b;
+                    pixels[y, x, 3] = a;
+                }
+            }
+
+            // manually set one pixel
+            pixels[5, 7, 0] = 255; // red
+            pixels[5, 7, 1] = 255; // gren
+            pixels[5, 7, 2] = 255; // blue
+            pixels[5, 7, 3] = 255; // alpha
+
+            byte[] bytes = IO.BmpWriter.GetBitmapBytes(pixels);
+            string saveAs = Path.GetFullPath("test.bmp");
+            System.IO.File.WriteAllBytes(saveAs, bytes);
+            Console.WriteLine(saveAs);
+        }
+    }
+}
---
+++ src/SciTIF/IO/BmpWriter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SciTIF.IO
+{
+    public static class BmpWriter
+    {
+        /// <summary>
+        /// Create a BMP file from an array of pixel values (Y, X, RGBA Color)
+        /// </summary>
+        public static byte[] GetBitmapBytes(byte[,,] pixels)
+        {
+            int height = pixels.GetLength(0);
+            int width = pixels.GetLength(1);
+            int bytesPerPixel = pixels.GetLength(2);
+            if (bytesPerPixel != 4)
+                throw new ArgumentException("4 color bytes required");
+            int imageBytesSize = width * height * bytesPerPixel;
+            const int imageHeaderSize = 54;
+            const int dbHeaderSize = 40;
+
+            byte[] bytes = new byte[imageBytesSize + imageHeaderSize];
+            bytes[0] = (byte)'B';
+            bytes[1] = (byte)'M';
+            bytes[14] = dbHeaderSize;
+
+            Array.Copy(BitConverter.GetBytes(bytes.Length), 0, bytes, 2, 4);
+            Array.Copy(BitConverter.GetBytes(imageHeaderSize), 0, bytes, 10, 4);
+            Array.Copy(BitConverter.GetBytes(width), 0, bytes, 18, 4);
+            Array.Copy(BitConverter.GetBytes(height), 0, bytes, 22, 4);
+            Array.Copy(BitConverter.GetBytes(bytesPerPixel * 8), 0, bytes, 28, 2);
+            Array.Copy(BitConverter.GetBytes(imageBytesSize), 0, bytes, 34, 4);
+
+            for (int y = 0; y < height; y++)
+            {
+                int rowOffset = (height - 1 - y) * (width * bytesPerPixel);
+                for (int x = 0; x < width; x++)
+                {
+                    int offset = rowOffset + x * bytesPerPixel + imageHeaderSize;
+                    bytes[offset + 2] = pixels[y, x, 0];
+                    bytes[offset + 1] = pixels[y, x, 1];
+                    bytes[offset + 0] = pixels[y, x, 2];
+                    bytes[offset + 3] = pixels[y, x, 3];
+                }
+            }
+
+            return bytes;
+        }
+    }
+}
